33a04ca7caec77c69fce5b8401dde26431717b59
Neto test <annav.rybina@yandex.ru>
2026-02-28 17:11:35 +0300

    Создание структуры проекта

---
+++ credit-application-service/src/main/java/com/github/annarybina/credit_application_service/CreditApplicationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.annarybina.credit_application_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditApplicationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditApplicationServiceApplication.class, args);
+	}
+
+}
---
+++ credit-decision-service/src/main/java/com/github/annarybina/credit_decision_service/CreditDecisionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.annarybina.credit_decision_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditDecisionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditDecisionServiceApplication.class, args);
+	}
+
+}
